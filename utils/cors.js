c0f7c06d55601c640acddcd9663c3cd6c32bcfb5
SemenovDmitrii <semenov.dmitrii.s.d.v@yandex.ru>
2022-10-31 20:43:20 +0300

    Update cors.js

--- utils/cors.js
+++ utils/cors.js
@@ -1,11 +1,30 @@
-const domainCors = {
-  origin: [
-    'http://api.sdv.nomoredomains.icu',
-    'https://api.sdv.nomoredomains.icu',
-    'http://localhost:3000',
-    'http://localhost:3001',
-  ],
-  credentials: true,
+const domainCors = [
+  'http://sdv.nomoredomains.sbs.nomoredomains.icu',
+  'https://sdv.nomoredomains.sbs.nomoredomains.icu',
+  'http://api.sdv.nomoredomains.icu',
+  'https://api.sdv.nomoredomains.icu',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (domainCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
 };
 
 module.exports = domainCors;
